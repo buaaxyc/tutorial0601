93dd43508c0eb629d3fc5f00bf575fac889cf8b7
buaaxyc <49428606+buaaxyc@users.noreply.github.com>
2020-06-12 08:22:06 +0800

    200612 0821

---
+++ salsa_practice/Assets/Scripts/EmoteR_ManualEmote.cs
@@ -0,0 +1,19 @@
+﻿using CrazyMinnow.SALSA;
+using UnityEngine;
+
+public class EmoteR_ManualEmote : MonoBehaviour
+{
+    public Emoter emoter;
+    private bool emoteState = false;
+
+    public void TriggerEmote(string emoteName)
+    {
+        emoter.ManualEmote(emoteName, ExpressionComponent.ExpressionHandler.RoundTrip, 5f);
+    }
+
+    public void ToggleEmote(string emoteName)
+    {
+        emoteState = !emoteState;
+        emoter.ManualEmote(emoteName, ExpressionComponent.ExpressionHandler.OneWay, 0f, emoteState);
+    }
+}
---
+++ salsa_practice/Assets/Scripts/Position_transport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position_transport : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void PositionTransport()
+    {
+        obj.transform.TransformPoint(Vector3.zero);
+        Debug.Log("yyy");
+    }
+}
